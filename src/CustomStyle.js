7425ee30ba4f6dcc64977ef4e36a8d5049acc908
Sarah Pak <spak@spotify.com>
2021-09-20 02:39:46 -0400

    restored customStyle.js to the way it was

--- src/CustomStyle.js
+++ src/CustomStyle.js
@@ -1,24 +1,39 @@
-import React, { Component, useEffect } from "react";
-import { Shaders, Node, GLSL } from "gl-react";
+import React, { PureComponent, Component } from "react";
+import { Shaders, Node, GLSL, LinearCopy } from "gl-react";
 import { Surface } from "gl-react-dom";
 import JSON2D from "react-json2d";
-import MersenneTwister from 'mersenne-twister';
+import timeLoop from "../src/HOC/timeLoop";
 
 const shaders = Shaders.create({
-  funky: {
+  animated: {
     frag: GLSL`
 precision highp float;
 varying vec2 uv;
 uniform sampler2D t;
-void main() {
-  gl_FragColor = texture2D(t, uv) * vec4(
-    0.5 + 0.5 * cos(uv.x * 30.0),
-    0.5 + 0.5 * sin(uv.y * 20.0),
-    0.7 + 0.3 * sin(uv.y * 8.0),
-    1.0);
+uniform float time, amp, freq, colorSeparation, moving;
+vec2 lookup (vec2 offset) {
+  return mod(
+    uv + amp * vec2(
+        cos(freq*(uv.x+offset.x)+time/1000.0),
+        sin(freq*(uv.y+offset.x)+time/1000.0))
+      + vec2( moving * time/10000.0, 0.0),
+    vec2(1.0));
 }
-` }
+void main() {
+  vec3 col =  mix(vec3(
+    texture2D(t, lookup(vec2(colorSeparation))).r,
+    texture2D(t, lookup(vec2(-colorSeparation))).g,
+    texture2D(t, lookup(vec2(0.0))).b),  vec3(1.0), 0.1);
+  gl_FragColor = vec4(col * vec3(
+    0.5 + 0.5 * cos(uv.y + uv.x * 49.0),
+    0.6 * uv.x + 0.2 * sin(uv.y * 30.0),
+    1.0 - uv.x + 0.5 * cos(uv.y * 2.0)
+  ), 1.0);
+}
+`
+  }
 });
+
 const Funky = ({children: t}) => <Node shader={shaders.funky} uniforms={{t}} />;
 
 class CustomStyle extends Component {
