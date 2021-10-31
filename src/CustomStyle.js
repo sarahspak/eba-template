b5c508655d369c9a006241f8ffa07335606d6d16
Sarah Pak <spak@spotify.com>
2021-10-31 08:00:51 -0400

    updated to actually do my own thing

--- src/CustomStyle.js
+++ src/CustomStyle.js
@@ -23,6 +23,10 @@ Getting started:
 */
 
 let DEFAULT_SIZE = 500;
+const Y_AXIS = 1;
+const X_AXIS = 2;
+let b1, b2, c1, c2;
+
 const CustomStyle = ({
   block,
   canvasRef,
@@ -44,8 +48,15 @@ const CustomStyle = ({
   const setup = (p5, canvasParentRef) => {
     // Keep reference of canvas element for snapshots
     p5.createCanvas(width, height).parent(canvasParentRef);
+    // Define colors
+    b1 = p5.color(255);
+    b2 = p5.color(0);
+    c1 = p5.color(204, 102, 0);
+    c2 = p5.color(0, 102, 153);
+    p5.noLoop();
+    
+    
     canvasRef.current = p5;
-
     attributesRef.current = () => {
       return {
         // This is called when the final image is generated, when creator opens the Mint NFT modal.
@@ -68,7 +79,8 @@ const CustomStyle = ({
       };
     };
   };
-
+  
+  
   // draw() is called right after setup and in a loop
   // disabling the loop prevents controls from working correctly
   // code must be deterministic so every loop instance results in the same output
@@ -101,15 +113,59 @@ const CustomStyle = ({
     // example assignment of hoisted value to be used as NFT attribute later
     hoistedValue.current = 42;
 
-    objs.forEach((dot, i) => {
-      p5.stroke(color1);
-      p5.strokeWeight(1 + mod2 * 10);
-      p5.ellipse(
-        200 * dot.y * 6 * M,
-        100 * dot.x * 6 * M,
-        dot.radius * M * mod1
-      );
-    });
+    p5.fill(255, 255, 255);
+    p5.stroke(255, 255, 255);
+    objs.forEach((vals, index) => {
+        var i = vals.radius * mod1;
+        var w = vals.radius;
+        var from = p5.map(vals.x, 0, .25, 0, width)
+        console.log("from is ")
+        console.log(from);
+        var to = p5.map(vals.y, 0, .25, 0, height)
+
+
+
+        var weight = p5.map(vals.radius, 0, 100, 0, 2);
+        p5.strokeWeight(weight/2);
+        
+        p5.ellipse(from, to, from/10 + vals.x + w, i);
+        p5.strokeWeight(weight);
+
+        p5.line(from, vals.radius*10, from + vals.x + w, i);
+
+    
+    }
+    )
+    
+    // // Background
+    // setGradient(0, 0, WIDTH / 2, HEIGHT, b1, b2, X_AXIS);
+    // setGradient(WIDTH / 2, 0, WIDTH / 2, HEIGHT, b2, b1, X_AXIS);
+    // // Foreground
+    // setGradient(50, 90, 540, 80, c1, c2, Y_AXIS);
+    // setGradient(50, 190, 540, 80, c2, c1, X_AXIS);
+
+    // function setGradient(x, y, w, h, c1, c2, axis) {
+    //     p5.noFill();
+      
+    //     if (axis === Y_AXIS) {
+    //       // Top to bottom gradient
+    //       for (let i = y; i <= y + h; i++) {
+    //         let inter = p5.map(i, y, y + h, 0, 1);
+    //         let c = p5.lerpColor(color1, background, inter);
+    //         p5.stroke(c);
+    //         p5.line(x, i, x + w, i);
+    //       }
+    //     } else if (axis === X_AXIS) {
+    //       // Left to right gradient
+    //       for (let i = x; i <= x + w; i++) {
+    //         let inter = p5.map(i, x, x + w, 0, 1);
+    //         let c = p5.lerpColor(color1, background, inter);
+    //         p5.stroke(c);
+    //         p5.line(i, y, i, y + h);
+    //       }
+    //     }
+    //   }
+
   };
 
   return <Sketch setup={setup} draw={draw} windowResized={handleResize} />;
